4ae1cb38614979d5c00c3c61b02819561feb4615
曹真 <Zhen@172-16-217-121.DYNAPOOL.NYU.EDU>
2016-04-20 23:52:38 -0400

    This is the new project

---
+++ Assets/floating.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class floating : MonoBehaviour {
+	public float FloatStrength;
+	public float RandomRotationStrength;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FloatStrength = Random.Range (0, 5f);
+		RandomRotationStrength = Random.Range (0, 10f);
+		GetComponent<Rigidbody>().AddForce (Vector3.up * FloatStrength);
+		transform.Rotate (RandomRotationStrength, RandomRotationStrength, RandomRotationStrength);
+	}
+}
